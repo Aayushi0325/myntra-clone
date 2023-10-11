8384b673d19dc741db3d1a38d30e3fa6be2c8611
Aayushi0325 <113809865+Aayushi0325@users.noreply.github.com>
2023-10-11 09:09:06 +0530

    Add files via upload

---
+++ myntra/Common Files/hyperlinks.js
@@ -0,0 +1,21 @@
+
+document.querySelector('.fa_user').addEventListener('click', function(){
+    window.location.href = "../Profile/profile.html"
+})
+// HYPERLINKS
+document.querySelector('.fa-heart').addEventListener('click', function(){
+    window.location.href = "../wishlist/wishlist.html"
+})
+document.querySelector(".fa_wishlist").addEventListener('click', function(){
+    window.location.href = "../wishlist/wishlist.html"
+})
+document.querySelector('.fa-bag-shopping').addEventListener('click', function(){
+    window.location.href = "../wishlist/cart.html"
+})
+document.querySelector('.fa_cart').addEventListener('click', function(){
+    window.location.href = "../wishlist/cart.html"
+})
+
+document.getElementById('landingPage').addEventListener('click', function(){
+    window.location.href = "../index.html"
+})
